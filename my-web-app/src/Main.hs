ac819e54e646f81985d774992b826485bfff982a
Meta77 <horokusa@keio.jp>
2025-05-23 23:34:21 +0900

    エラーの確認、実験

--- my-web-app/src/Main.hs
+++ my-web-app/src/Main.hs
@@ -6,8 +6,8 @@ import Web.Scotty
 import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text as T
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.Text.Encoding as TE
+-- import qualified Data.ByteString.Lazy as BL
+-- import qualified Data.Text.Encoding as TE
 
 main :: IO ()
 main = scotty 3000 $ do -- ポート3000番でWebサーバーを起動する関数
@@ -55,12 +55,13 @@ main = scotty 3000 $ do -- ポート3000番でWebサーバーを起動する関�
     name <- param "name"
     text $ "Hello, " <> (name :: Text) <> "!"
 
+{-
   -- POST /echo
   post "/echo" $ do
     bodyBytes <- body               -- ByteString Lazy
     let bodyText = TL.fromStrict $ TE.decodeUtf8 $ BL.toStrict bodyBytes
     text $ "You posted: " <> bodyText
-
+-}
 
   {-
   -- GET /form → フォーム表示
